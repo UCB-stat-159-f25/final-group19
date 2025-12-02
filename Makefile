1b4ac373683253ba413f63d53123e93cc14cf4ea
Calvin Carter <calv2n@berkeley.edu>
2025-12-01 21:31:30 -0800

    Added Makefile, code to create data from html.

---
+++ Makefile
@@ -0,0 +1,16 @@
+ENV_NAME = inaugural-address
+PYTHON = python
+
+.PHONY: all env data
+
+all: env data
+
+env:
+	@if conda env list | grep -q "^$(ENV_NAME) "; then \
+		conda env update -n $(ENV_NAME) -f environment.yaml; \
+	else \
+		conda env create -f environment.yaml; \
+	fi
+
+data:
+	$(PYTHON) make_data.py
---
+++ make_data.py
@@ -0,0 +1,54 @@
+from bs4 import BeautifulSoup
+import pandas as pd
+import numpy as np
+import requests
+import time
+import re
+
+def write_inaugural_addresses():
+    base_url = "https://www.presidency.ucsb.edu/documents/inaugural-address"
+    
+    speech_pres_name = np.empty(55, dtype=object)
+    speech_pres_num = np.empty(55, dtype=object)
+    speech_date = np.empty(55, dtype=object)
+    speech_content = np.empty(55, dtype=object)
+
+    for i in range(0, 55):
+        curr_url = base_url + '-' + str(i) if i > 0 else base_url
+        response = requests.get(curr_url, timeout=10)
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        h3 = soup.find('h3', class_='diet-title')
+        pres_name = h3.find('a').get_text()
+        speech_pres_name[i] = pres_name
+        
+        pres_number_str = soup.find('div', class_="diet-by-line president").get_text(strip=True)
+        pres_number = int(re.search("[0-9][0-9]?", pres_number_str).group())
+        speech_pres_num[i] = pres_number
+
+        date_span = soup.find('span', class_='date-display-single')['content']
+        date = pd.to_datetime(date_span)
+        speech_date[i] = date
+
+        content_div = soup.find('div', class_='field-docs-content')
+        content = content_div.get_text()
+        speech_content[i] = content
+
+        time.sleep(0.05) # don't make the api cry
+
+    # sorting because they are out of order in url for some reason?
+    colnames = ['president_name', 'president_number', 'date', 'text']
+    inaugural_address = pd.DataFrame(
+        np.column_stack([
+            speech_pres_name,
+            speech_pres_num,
+            speech_date,
+            speech_content
+        ]),
+        columns=colnames
+    ).sort_values('date').reset_index(drop=True)
+    
+    inaugural_address.to_csv("data/inaugural_address.csv")
+
+if __name__ == "__main__":
+    write_inaugural_addresses()
